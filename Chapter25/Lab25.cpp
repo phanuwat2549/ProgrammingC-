858a0d64d403a728744be44871b602e35f619328
phanuwat2549 <panuwat2007e@gmail.com>
2025-08-05 15:02:29 +0700

    Update Lab25.cpp

--- Chapter25/Lab25.cpp
+++ Chapter25/Lab25.cpp
@@ -1,1 +1,37 @@
+#include <iostream>
+#include <cmath> 
 
+class Dcircle {
+private:
+    double radius;
+
+public:
+   
+    Dcircle() {
+        radius = 1.0;
+    }
+
+    Dcircle(double r) {
+        radius = r;
+    }
+
+    void findArea() const {
+        double area = M_PI * radius * radius;
+        std::cout << "Area of circle: " << area << std::endl;
+    }
+
+    void perimeter() const {
+        double circ = 2 * M_PI * radius;
+        std::cout << "Perimeter (Circumference) of circle: " << circ << std::endl;
+    }
+};
+
+int main() {
+	
+    Dcircle tr3;
+
+    tr3.findArea();
+    tr3.perimeter();
+
+    return 0;
+}
